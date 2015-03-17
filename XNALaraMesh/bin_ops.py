5c839f82ab98152202a6892a28ea83a4dff50022
Sebastian Brachi <sebastianbrahi@gmail.com>
2015-03-17 15:54:16 -0300

    pep8 compliant. Fixed: E265 block comment should start with '# ' E302 expected 2 blank lines, found 1 E225 missing whitespace around operator E231 missing whitespace after ',' W391 blank line at end of file

--- XNALaraMesh/bin_ops.py
+++ XNALaraMesh/bin_ops.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from XNALaraMesh import xps_const
-
 import struct
 
-#Format
+from XNALaraMesh import xps_const
+
+
+# Format
 class TypeFormat:
     SByte = '<b'
     Byte = '<B'
@@ -17,84 +18,98 @@ class TypeFormat:
     Single = '<f'
     Double = '<d'
 
+
 def roundToMultiple(numToRound, multiple):
     remainder = numToRound % multiple
     if (remainder == 0):
         return numToRound
     return numToRound + multiple - remainder
 
+
 def readByte(file):
     numberBin = file.read(1)
     number = struct.unpack(TypeFormat.Byte, numberBin)[0]
     return number
 
+
 def writeByte(number):
     bytesBin = struct.pack(TypeFormat.Byte, number)
     return bytesBin
+
 
 def readUInt16(file):
     numberBin = file.read(2)
     number = struct.unpack(TypeFormat.UInt16, numberBin)[0]
     return number
 
+
 def writeUInt16(number):
     uInt16 = struct.pack(TypeFormat.UInt16, number)
     return uInt16
+
 
 def readInt16(file):
     numberBin = file.read(2)
     number = struct.unpack(TypeFormat.Int16, numberBin)[0]
     return number
 
+
 def writeInt16(number):
     int16 = struct.pack(TypeFormat.Int16, number)
     return int16
+
 
 def readUInt32(file):
     numberBin = file.read(4)
     number = struct.unpack(TypeFormat.UInt32, numberBin)[0]
     return number
 
+
 def writeUInt32(number):
     uInt32 = struct.pack(TypeFormat.UInt32, number)
     return uInt32
+
 
 def readSingle(file):
     numberBin = file.read(4)
     single = struct.unpack(TypeFormat.Single, numberBin)[0]
     return single
 
+
 def writeSingle(number):
     single = struct.pack(TypeFormat.Single, number)
     return single
 
+
 def readString(file, length):
     try:
-        pos1=file.tell()
+        pos1 = file.tell()
         byteString = file.read(length)
-        pos2=file.tell()
-        string=''
+        pos2 = file.tell()
+        string = ''
         string = decodeBytes(byteString)
     except Exception:
-        print('*'*40)
-        print('pos len',pos1)
-        print('pos str',pos2)
-        print('pos',file.tell())
-        print('len',length)
-        print('str',byteString)
+        print('*' * 40)
+        print('pos len', pos1)
+        print('pos str', pos2)
+        print('pos', file.tell())
+        print('len', length)
+        print('str', byteString)
         string = decodeBytes(byteString)
     return string
 
+
 def writeString(string):
-    #String Lenght
+    # String Lenght
     byteString = encodeString(string)
     return byteString
 
+
 def decodeBytes(bytes):
-    #print(bytes)
+    # print(bytes)
     return bytes.decode(xps_const.ENCODING_READ)
 
-def encodeString(string):
-    #print(string)
-    return string.encode(xps_const.ENCODING_WRITE)
 
+def encodeString(string):
+    # print(string)
+    return string.encode(xps_const.ENCODING_WRITE)
