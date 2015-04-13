b14ed977b7db9b19fe85e106a77e09a4d79885eb
johnzero7 <johnzero7@gmail.com>
2015-04-12 23:04:07 -0300

    Version number
    I always forget to update it...

--- XNALaraMesh/__init__.py
+++ XNALaraMesh/__init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "XNALara/XPS Import/Export",
     "author": "johnzero7",
-    "version": (1, 5, 0),
+    "version": (1, 5, 1),
     "blender": (2, 7, 0),
     "location": "File > Import/Export > XNALara/XPS",
     "description": "Import-Export XNALara/XPS",
