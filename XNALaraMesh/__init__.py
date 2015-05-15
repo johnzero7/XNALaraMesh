a86622473be830a90d0d82326ee757f9f24829c3
johnzero7 <johnzero7@gmail.com>
2015-05-14 21:36:57 -0300

    Minimum Blender requiered 2.7.4

--- XNALaraMesh/__init__.py
+++ XNALaraMesh/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "XNALara/XPS Import/Export",
     "author": "johnzero7",
     "version": (1, 5, 2),
-    "blender": (2, 7, 0),
+    "blender": (2, 7, 4),
     "location": "File > Import/Export > XNALara/XPS",
     "description": "Import-Export XNALara/XPS",
     "warning": "",
