6622c2b9365deac0c0931536b52c84e53795d28d
johnzero7 <johnzero7@gmail.com>
2018-01-14 02:28:05 -0300

    Update ver

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 bl_info = {
     "name": "XNALara/XPS Import/Export",
     "author": "johnzero7",
-    "version": (1, 7, 4),
+    "version": (1, 8, 0),
     "blender": (2, 74, 0),
     "location": "File > Import-Export > XNALara/XPS",
     "description": "Import-Export XNALara/XPS",
